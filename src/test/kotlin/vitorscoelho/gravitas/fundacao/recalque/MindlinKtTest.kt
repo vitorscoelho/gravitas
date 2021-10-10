e73bdbc1cedeea5b5779ccbe3e825dbd5f90bb00
vitorscoelho <vitor.eec@hotmail.com>
2021-10-10 16:57:11 -0300

    Adicionado novo teste. Removido o delta de comparação das respostas de recalques

--- src/test/kotlin/vitorscoelho/gravitas/fundacao/recalque/MindlinKtTest.kt
+++ src/test/kotlin/vitorscoelho/gravitas/fundacao/recalque/MindlinKtTest.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import vitorscoelho.gravitas.utils.gpa
 import vitorscoelho.gravitas.utils.metros
+import vitorscoelho.gravitas.utils.mpa
+import kotlin.math.pow
 
 internal class MindlinKtTest {
     /**
@@ -57,9 +59,8 @@ internal class MindlinKtTest {
             pontosRecalque = pontosRecalque,
             camadas = camadas
         )
-        val delta = 0.000001
-        assertEquals(0.031645, recalquePelasForcasDoFuste[pontoRecalque1]!!, delta)//Alonso=0.033
-        assertEquals(0.851887, recalquePelasForcasDaPonta[pontoRecalque1]!!, delta)//Alonso=0.852
+        assertEquals(0.03172500123025457, recalquePelasForcasDoFuste[pontoRecalque1]!!)//Alonso=0.033
+        assertEquals(0.8518878807539629, recalquePelasForcasDaPonta[pontoRecalque1]!!)//Alonso=0.852
     }
 
     /**
@@ -119,13 +120,79 @@ internal class MindlinKtTest {
             forcas = forcasDePonta, pontosRecalque = pontosRecalque, camadas = camadas
         )
 
-        val delta = 0.000001
         //Ponto1
-        assertEquals(0.190538, recalquePelasForcasDeFuste[pontoRecalque1]!!, delta)//Alonso=0.199
-        assertEquals(1.142798, recalquePelasForcasDePonta[pontoRecalque1]!!, delta)//Alonso=1.139
+        assertEquals(0.1905384203535907, recalquePelasForcasDeFuste[pontoRecalque1]!!)//Alonso=0.199
+        assertEquals(1.142798093519414, recalquePelasForcasDePonta[pontoRecalque1]!!)//Alonso=1.139
 
         //Ponto2
-        assertEquals(0.071339, recalquePelasForcasDeFuste[pontoRecalque2]!!, delta)//Alonso=0.073
-        assertEquals(1.036229, recalquePelasForcasDePonta[pontoRecalque2]!!, delta)//Alonso=1.036
+        assertEquals(0.07133995730202591, recalquePelasForcasDeFuste[pontoRecalque2]!!)//Alonso=0.073
+        assertEquals(1.0362294980271745, recalquePelasForcasDePonta[pontoRecalque2]!!)//Alonso=1.036
+    }
+
+    /**
+     * Exemplo do Apêndice A do livro Fundações em Estacas de Bernadete Ragoni Danziger e Francisco De Rezende Lopes
+     */
+    @Test
+    fun exemploDanzigerELopes() {
+        val secao = SecaoCircular(
+            diametro = 60.0, divisoesCircunferenciaArea = 5, divisoesRaioArea = 5, divisoesPerimetro = 5
+        )
+
+        fun atritoLateral(pressaoLateralEmKnPorM2: Double) = pressaoLateralEmKnPorM2 * secao.perimetro / 100.0.pow(2)
+
+        val atrito1 = atritoLateral(10.0)
+        val atrito2 = atritoLateral(15.0)
+        val atrito3 = atritoLateral(25.0)
+        val trecho1 = TrechoDeEstaca(
+            secao = secao, moduloDeDeformacao = 2100.0,
+            qtdDivisoes = 10, atritoLateralTopo = atrito1, atritoLateralBase = atrito1,
+            comprimento = 10.metros
+        )
+        val trecho2 = TrechoDeEstaca(
+            secao = secao, moduloDeDeformacao = 2100.0,
+            qtdDivisoes = 10, atritoLateralTopo = atrito2, atritoLateralBase = atrito3,
+            comprimento = 10.metros
+        )
+
+        val estaca = EstacaPrismatica(
+            topo = Vetor3D.ZERO,
+            trechos = listOf(trecho1, trecho2),
+            cargaNaPonta = 434.5
+        )
+
+        val camada1 = Camada(profundidade = 10.metros, moduloDeYoung = 10.mpa, poisson = 0.3)
+        val camada2 = Camada(profundidade = 20.metros, moduloDeYoung = 15.mpa, poisson = 0.35)
+        val camada3 = Camada(profundidade = 25.metros, moduloDeYoung = 40.mpa, poisson = 0.4)
+        val camadas = listOf(camada1, camada2, camada3)
+
+        val pontoRecalque = Vetor3D.ZERO.copy(z = 20.metros)
+
+        val recalquePelasForcasDeFuste = recalqueMindlinSteinbrenner(
+            forcas = estaca.forcasDoFuste, pontosRecalque = listOf(pontoRecalque), camadas = camadas
+        )
+        val recalquePelasForcasDePonta = recalqueMindlinSteinbrenner(
+            forcas = estaca.forcasDaPonta, pontosRecalque = listOf(pontoRecalque), camadas = camadas
+        )
+
+        //Estaca isolada
+        assertEquals(0.06901327720536186, recalquePelasForcasDeFuste[pontoRecalque]!!)//No livro, é 0.08
+        assertEquals(0.8438271556356041, recalquePelasForcasDePonta[pontoRecalque]!!)
+
+        //Estaca central em grupo de nove estacas espaçadas em 180cm
+        val grupoDeEstacas = listOf(-180.0, 0.0, 180.0).flatMap { x ->
+            listOf(-180.0, 0.0, 180.0).map { y -> estaca.copy(topo = Vetor3D(x = x, y = y, z = 0.0)) }
+        }
+
+        val forcasFusteGrupo = grupoDeEstacas.asSequence().flatMap { it.forcasDoFuste }
+        val forcasPontaGrupo = grupoDeEstacas.asSequence().flatMap { it.forcasDaPonta }
+
+        val recalquePelasForcasDeFusteGrupo = recalqueMindlinSteinbrenner(
+            forcas = forcasFusteGrupo, pontosRecalque = listOf(pontoRecalque), camadas = camadas
+        )
+        val recalquePelasForcasDePontaGrupo = recalqueMindlinSteinbrenner(
+            forcas = forcasPontaGrupo, pontosRecalque = listOf(pontoRecalque), camadas = camadas
+        )
+        assertEquals(0.24786087499576462, recalquePelasForcasDeFusteGrupo[pontoRecalque]!!)//No livro, é 0.28
+        assertEquals(1.0585596726046522, recalquePelasForcasDePontaGrupo[pontoRecalque]!!)
     }
 }
